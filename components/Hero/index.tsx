4f8d374d55d580a8b16dfaf6476f3408e0d48e25
Mortez-usa <hatam.info@gmail.com>
2023-12-11 15:19:57 -0800

    add hero picture

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -8,6 +8,9 @@ const Hero = () => {
         id="home"
         className="relative z-10 overflow-hidden bg-white pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
       >
+        <Image src='./images/hero/hero-02.jpeg' fill className='z-[-10] brightness-50 'alt="" >
+
+        </Image>
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
