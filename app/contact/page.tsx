1995ade78346fb8206d3e0705abf01006d1f7515
Mortez-usa <hatam.info@gmail.com>
2023-12-11 16:10:35 -0800

    this is contact page updated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Contact Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Contact Page for Startup Nextjs Template",
+  description: "This is Contact Page",
   // other metadata
 };
 
