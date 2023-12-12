3f73fbbca430d1096012c2b93f9458ce16902603
Mortez-usa <hatam.info@gmail.com>
2023-12-12 11:21:21 -0800

    update name

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "About Page",
   description: "This is About Page for Docktorly",
-  // other metadata
+  // other metadata...
 };
 
 const AboutPage = () => {
