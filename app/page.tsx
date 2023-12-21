2707d481b00bc4d22c3e7b45536872bc30fce574
Mortez-usa <hatam.info@gmail.com>
2023-12-21 11:47:36 -0800

    env files

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,6 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-    {/* google tag */}
       <Script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
